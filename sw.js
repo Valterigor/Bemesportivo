ae87b1d6de9e5393235a726b93737b8391feec1b
Valterigor <valterigor@gmail.com>
2026-07-31 06:37:08 -0300

    fix: remove mockups de celulares da pagina

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'meu-caminho-be-v66';
+const CACHE_NAME = 'meu-caminho-be-v67';
 const APP_SHELL = [
   '/meu-caminho-be',
   '/site-common.css?v=20260723-3',
@@ -17,7 +17,7 @@ const APP_SHELL = [
   '/css/routine-calendar.css?v=20260722-1',
   '/css/ui-polish.css?v=20260723-3',
   '/css/meu-caminho-modern.css?v=20260729-5',
-  '/css/meu-caminho-diary.css?v=20260731-2',
+  '/css/meu-caminho-diary.css?v=20260731-3',
   '/js/site-common.js?v=20260723-3',
   '/js/core/routes.js',
   '/js/components/site-navigation.js?v=20260723-2',
@@ -33,7 +33,7 @@ const APP_SHELL = [
   '/js/coluna-valtinho.js?v=20260722-2',
   '/js/fala-bem-app.js?v=20260729-7',
   '/js/be-ia.js?v=20260729-1',
-  '/js/meu-caminho-diary.js?v=20260731-2',
+  '/js/meu-caminho-diary.js?v=20260731-3',
   '/img/logobemoficial.png',
   '/img/app-icon-192.png',
   '/img/app-icon-512.png',
